43b0abfe579e7bc25343938eda7e5e598d656ef3
TechQuery <shiy2008@gmail.com>
2025-07-26 06:08:10 +0800

    [add] Wiki types & models

--- src/module/Wiki/type.ts
+++ src/module/Wiki/type.ts
@@ -1,3 +1,10 @@
+export interface LarkWiki
+    extends Record<'name' | ' description' | 'space_id', string> {
+    space_type: 'team' | 'person' | 'my_library';
+    visibility: 'public' | 'private';
+    open_sharing: 'open' | 'closed';
+}
+
 export interface LarkWikiNode
     extends Record<
         | 'title'
