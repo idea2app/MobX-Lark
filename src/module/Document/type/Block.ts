162a2dd77eafd25c499597a804d33150d0a2d5e1
TechQuery <shiy2008@gmail.com>
2025-07-27 00:30:47 +0800

    [add] Text Block types of Lark Document

---
+++ src/module/Document/type/Block.ts
@@ -0,0 +1,226 @@
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#e8ce4e8e}
+ */
+export enum BlockType {
+    /**
+     * 页面 Block
+     */
+    page = 1,
+    /**
+     * 文本 Block
+     */
+    text = 2,
+    /**
+     * 标题 1 Block
+     */
+    heading1 = 3,
+    /**
+     * 标题 2 Block
+     */
+    heading2 = 4,
+    /**
+     * 标题 3 Block
+     */
+    heading3 = 5,
+    /**
+     * 标题 4 Block
+     */
+    heading4 = 6,
+    /**
+     * 标题 5 Block
+     */
+    heading5 = 7,
+    /**
+     * 标题 6 Block
+     */
+    heading6 = 8,
+    /**
+     * 标题 7 Block
+     */
+    heading7 = 9,
+    /**
+     * 标题 8 Block
+     */
+    heading8 = 10,
+    /**
+     * 标题 9 Block
+     */
+    heading9 = 11,
+    /**
+     * 无序列表 Block
+     */
+    bullet = 12,
+    /**
+     * 有序列表 Block
+     */
+    ordered = 13,
+    /**
+     * 代码块 Block
+     */
+    code = 14,
+    /**
+     * 引用 Block
+     */
+    quote = 15,
+    /**
+     * 待办事项 Block
+     */
+    todo = 17,
+    /**
+     * 多维表格 Block
+     */
+    bitable = 18,
+    /**
+     * 高亮块 Block
+     */
+    callout = 19,
+    /**
+     * 会话卡片 Block
+     */
+    chat_card = 20,
+    /**
+     * 流程图 & UML Block
+     */
+    diagram = 21,
+    /**
+     * 分割线 Block
+     */
+    divider = 22,
+    /**
+     * 文件 Block
+     */
+    file = 23,
+    /**
+     * 分栏 Block
+     */
+    grid = 24,
+    /**
+     * 分栏列 Block
+     */
+    grid_column = 25,
+    /**
+     * 内嵌 Block Block
+     */
+    iframe = 26,
+    /**
+     * 图片 Block
+     */
+    image = 27,
+    /**
+     * 开放平台小组件 Block
+     */
+    isv = 28,
+    /**
+     * 思维笔记 Block
+     */
+    mindnote = 29,
+    /**
+     * 电子表格 Block
+     */
+    sheet = 30,
+    /**
+     * 表格 Block
+     */
+    table = 31,
+    /**
+     * 表格单元格 Block
+     */
+    table_cell = 32,
+    /**
+     * 视图 Block
+     */
+    view = 33,
+    /**
+     * 引用容器 Block
+     */
+    quote_container = 34,
+    /**
+     * 任务 Block
+     */
+    task = 35,
+    /**
+     * OKR Block
+     */
+    okr = 36,
+    /**
+     * OKR Objective Block
+     */
+    okr_objective = 37,
+    /**
+     * OKR Key Result Block
+     */
+    okr_key_result = 38,
+    /**
+     * OKR Progress Block
+     */
+    okr_progress = 39,
+    /**
+     * 新版文档小组件 Block
+     */
+    add_ons = 40,
+    /**
+     * Jira 问题 Block
+     */
+    jira_issue = 41,
+    /**
+     * Wiki 子页面列表 Block（旧版）
+     */
+    wiki_catalog = 42,
+    /**
+     * 画板 Block
+     */
+    board = 43,
+    /**
+     * 议程 Block
+     */
+    agenda = 44,
+    /**
+     * 议程项 Block
+     */
+    agenda_item = 45,
+    /**
+     * 议程项标题 Block
+     */
+    agenda_item_title = 46,
+    /**
+     * 议程项内容 Block
+     */
+    agenda_item_content = 47,
+    /**
+     * 链接预览 Block
+     */
+    link_preview = 48,
+    /**
+     * 源同步块
+     */
+    source_synced = 49,
+    /**
+     * 引用同步块
+     */
+    reference_synced = 50,
+    /**
+     * Wiki 子页面列表 Block（新版）
+     */
+    sub_page_list = 51,
+    /**
+     * AI 模板 Block
+     */
+    ai_template = 52,
+    /**
+     * 未支持 Block
+     */
+    undefined = 999
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#8b7f3e74}
+ */
+export type Block<Type extends keyof typeof BlockType, Meta = {}> = {
+    block_id: string;
+    block_type: BlockType;
+    parent_id?: string;
+    children?: string[];
+    comment_ids?: string[];
+} & {
+    [type in Type]: Meta;
+};
---
+++ src/module/Document/type/Text.ts
@@ -0,0 +1,296 @@
+import { Block } from './Block';
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#05e7d57e}
+ */
+export enum Align {
+    left = 1,
+    center = 2,
+    right = 3
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#19b9b4bf}
+ */
+export type TextIndentationLevel = 'NoIndent' | 'OneLevelIndent';
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#be1b12a7}
+ */
+export enum FontColor {
+    Red = 1,
+    Orange = 2,
+    Yellow = 3,
+    Green = 4,
+    Blue = 5,
+    Purple = 6,
+    Gray = 7
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#45c9c07b}
+ */
+export enum FontBackgroundColor {
+    /**
+     * 浅红色
+     */
+    LightRedBackground = 1,
+    /**
+     * 浅橙色
+     */
+    LightOrangeBackground = 2,
+    /**
+     * 浅黄色
+     */
+    LightYellowBackground = 3,
+    /**
+     * 浅绿色
+     */
+    LightGreenBackground = 4,
+    /**
+     * 浅蓝色
+     */
+    LightBlueBackground = 5,
+    /**
+     * 浅紫色
+     */
+    LightPurpleBackground = 6,
+    /**
+     * 中灰色
+     */
+    PaleGrayBackground = 7,
+    /**
+     * 红色
+     */
+    DarkRedBackground = 8,
+    /**
+     * 橙色
+     */
+    DarkOrangeBackground = 9,
+    /**
+     * 黄色
+     */
+    DarkYellowBackground = 10,
+    /**
+     * 绿色
+     */
+    DarkGreenBackground = 11,
+    /**
+     * 蓝色
+     */
+    DarkBlueBackground = 12,
+    /**
+     * 紫色
+     */
+    DarkPurpleBackground = 13,
+    /**
+     * 灰色
+     */
+    DarkGrayBackground = 14,
+    /**
+     * 浅灰色
+     */
+    LightGrayBackground = 15
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#80293bd9}
+ */
+export type TextBackgroundColor = keyof typeof FontBackgroundColor;
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#45accc42}
+ */
+export enum CodeLanguage {
+    PlainText = 1,
+    ABAP = 2,
+    Ada = 3,
+    Apache = 4,
+    Apex = 5,
+    Assembly = 6,
+    Bash = 7,
+    CSharp = 8,
+    CPlusPlus = 9,
+    C = 10,
+    COBOL = 11,
+    CSS = 12,
+    CoffeeScript = 13,
+    D = 14,
+    Dart = 15,
+    Delphi = 16,
+    Django = 17,
+    Dockerfile = 18,
+    Erlang = 19,
+    Fortran = 20,
+    FoxPro = 21,
+    Go = 22,
+    Groovy = 23,
+    HTML = 24,
+    HTMLBars = 25,
+    HTTP = 26,
+    Haskell = 27,
+    JSON = 28,
+    Java = 29,
+    JavaScript = 30,
+    Julia = 31,
+    Kotlin = 32,
+    LateX = 33,
+    Lisp = 34,
+    Logo = 35,
+    Lua = 36,
+    MATLAB = 37,
+    Makefile = 38,
+    Markdown = 39,
+    Nginx = 40,
+    Objective = 41,
+    OpenEdgeABL = 42,
+    PHP = 43,
+    Perl = 44,
+    PostScript = 45,
+    PowerShell = 46,
+    Prolog = 47,
+    ProtoBuf = 48,
+    Python = 49,
+    R = 50,
+    RPG = 51,
+    Ruby = 52,
+    Rust = 53,
+    SAS = 54,
+    SCSS = 55,
+    SQL = 56,
+    Scala = 57,
+    Scheme = 58,
+    Scratch = 59,
+    Shell = 60,
+    Swift = 61,
+    Thrift = 62,
+    TypeScript = 63,
+    VBScript = 64,
+    Visual = 65,
+    XML = 66,
+    YAML = 67,
+    CMake = 68,
+    Diff = 69,
+    Gherkin = 70,
+    GraphQL = 71,
+    OpenGL_Shading_Language = 72,
+    Properties = 73,
+    Solidity = 74,
+    TOML = 75
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#1c6877f3}
+ */
+export interface TextStyle
+    extends Partial<Record<'done' | 'folded' | 'wrap', boolean>> {
+    align?: Align;
+    language?: CodeLanguage;
+    background_color?: TextBackgroundColor;
+    indentation_level?: TextIndentationLevel;
+    sequence?: string;
+}
+
+export interface TextElementStyle
+    extends Partial<
+        Record<
+            'bold' | 'italic' | 'strikethrough' | 'underline' | 'inline_code',
+            boolean
+        >
+    > {
+    text_color?: FontColor;
+    background_color?: FontBackgroundColor;
+    link?: { url: string };
+    comment_ids?: string[];
+}
+
+export interface TextElementData {
+    text_element_style?: TextElementStyle;
+}
+
+export interface TextRun extends TextElementData {
+    content: string;
+}
+
+export interface MentionUser extends TextElementData {
+    user_id: string;
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#90567af6}
+ */
+export enum MentionObjType {
+    Doc = 1,
+    Sheet = 3,
+    Bitable = 8,
+    MindNote = 11,
+    File = 12,
+    Slide = 15,
+    Wiki = 16,
+    Docx = 22
+}
+
+export interface MentionDoc extends TextElementData {
+    token: string;
+    obj_type: MentionObjType;
+    url: string;
+}
+
+export interface Reminder extends TextElementData {
+    create_user_id: string;
+    is_notify?: boolean;
+    is_whole_day?: boolean;
+    expire_time: number;
+    notify_time: number;
+}
+
+export interface InlineFile extends TextElementData {
+    file_token?: string;
+    source_block_id?: string;
+}
+
+export interface InlineBlock extends TextElementData {
+    block_id?: string;
+}
+
+export type Equation = TextRun;
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#68268d96}
+ */
+export interface TextElement {
+    text_run?: TextRun;
+    mention_user?: MentionUser;
+    mention_doc?: MentionDoc;
+    reminder?: Reminder;
+    file?: InlineFile;
+    inline_block?: InlineBlock;
+    equation?: Equation;
+    undefined_element?: {};
+}
+
+/**
+ * @see {@link https://open.feishu.cn/document/docs/docs/data-structure/block#c1ebd2a2}
+ */
+export type TextBlock = Block<
+    'text',
+    {
+        elements: TextElement[];
+        style: TextStyle;
+    }
+>;
+export type PageBlock = TextBlock;
+export type Heading1Block = TextBlock;
+export type Heading2Block = TextBlock;
+export type Heading3Block = TextBlock;
+export type Heading4Block = TextBlock;
+export type Heading5Block = TextBlock;
+export type Heading6Block = TextBlock;
+export type Heading7Block = TextBlock;
+export type Heading8Block = TextBlock;
+export type Heading9Block = TextBlock;
+export type BulletBlock = TextBlock;
+export type OrderedBlock = TextBlock;
+export type CodeBlock = TextBlock;
+export type QuoteBlock = TextBlock;
+export type TodoBlock = TextBlock;
