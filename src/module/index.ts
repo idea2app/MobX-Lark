6d88572e8909d2edaf858236c0a5acfeb4836173
TechQuery <shiy2008@gmail.com>
2025-07-27 00:30:47 +0800

    [add] Text Block types of Lark Document

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,6 +1,7 @@
 export * from './base';
 export * from './InstantMessenger';
 export * from './Task';
+export * from './Document/type';
 export * from './SpreadSheet';
 export * from './BITable';
 export * from './Wiki';
